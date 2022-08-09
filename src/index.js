a197a7660f8319b3473dadf31a81f90da6a1aafb
anasse Laarabi <anas@MacBook-Pro-de-anasse.local>
2022-08-09 19:52:56 +0100

    updating Calculator.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById('root'));
