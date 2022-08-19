062ded3010425935a893467d98d27de1e7c3a122
anasse Laarabi <anas@MacBook-Pro-de-anasse.local>
2022-08-19 19:28:54 +0100

    Creating operate.test.js

--- src/test/component.test.js
+++ src/test/component.test.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import Calculation from '../components/Calculation';
 import Calculator from '../components/calculator';
 import Home from '../components/Home';
 import Quotes from '../components/Quotes';
-describe('', () => {
-  it('', () => {
+
+describe('test components with snapshot', () => {
+  it('test Calculation component with snapshot', () => {
+    const tree = renderer.create(<Calculation />);
+    expect(tree).toMatchSnapshot();
+  });
+  it('test Calculator component with snapshot', () => {
     const tree = renderer.create(<Calculator />);
     expect(tree).toMatchSnapshot();
   });
-  it('', () => {
+  it('test Home component with snapshot', () => {
     const tree = renderer.create(<Home />);
     expect(tree).toMatchSnapshot();
   });
-  it('', () => {
-    const tree = renderer.create(<Quote />);
+  it('test Quotes component with snapshot', () => {
+    const tree = renderer.create(<Quotes />);
     expect(tree).toMatchSnapshot();
   });
+
 });
---
+++ src/test/operate.test.js
@@ -0,0 +1,9 @@
+import operate from "../logic/operate";
+
+describe('test the logic of the application', () => {
+  it ('take two numbers and return the operation of them', () =>{
+    expect(operate(2,5, '+')).toBe('7');
+    expect(operate(2,5, '-')).toBe('-3');
+    expect(operate(2,5, 'x')).toBe('10');
+  });
+});
