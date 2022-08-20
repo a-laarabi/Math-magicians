35f4026c395761829466b76b1d66c2f82dd0e947
anasse Laarabi <anas@MacBook-Pro-de-anasse.local>
2022-08-20 16:39:01 +0100

    Creating calculate.test.js & updating operate.test.js

--- src/test/operate.test.js
+++ src/test/operate.test.js
@@ -1,9 +1,19 @@
 import operate from '../logic/operate';
 
 describe('test the logic of the application', () => {
-  it('take two numbers and return the operation of them', () => {
+  it('Test for addition operation', () => {
     expect(operate(2, 5, '+')).toBe('7');
+  });
+  it('Test for subtraction operation', () => {
     expect(operate(2, 5, '-')).toBe('-3');
+  });
+  it('Test for multiplication operation', () => {
     expect(operate(2, 5, 'x')).toBe('10');
+  });
+  it('Test for division operation', () => {
+    expect(operate(5, 2, '÷')).toBe('2.5');
+  });
+  it('Test for modulus operation', () => {
+    expect(operate(2, 5, '%')).toBe('2');
   });
 });
